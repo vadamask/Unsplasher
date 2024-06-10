c038caec6cc566c49a15af7358e391f3924a2871
Vadim Shishkov <vadamask@gmail.com>
2024-06-10 19:00:12 +0300

    add project without storyboard

---
+++ Unsplasher/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Unsplasher
+//
+//  Created by Вадим Шишков on 10.06.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Unsplasher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplasher
+//
+//  Created by Вадим Шишков on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
